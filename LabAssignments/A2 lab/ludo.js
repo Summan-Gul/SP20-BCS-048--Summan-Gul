f634349db13671cb6ab293caafbf9fff5e909af0
Summan-Gul <114242474+Summan-Gul@users.noreply.github.com>
2022-11-04 23:23:46 +0500

    Lab Assignment 2 (Ludo)

---
+++ LabAssignments/A2 lab/ludo.js
@@ -0,0 +1,385 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Ludo() {
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.row}>
+      <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+        <View style={styles.redBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.whiteBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blueBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.blackBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+      </View>
+      <View style={styles.row}>
+      <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+      </View>
+
+      <View style={styles.row}>
+      <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.yellowBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.whiteBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+        <View style={styles.greenBox}></View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: 'brown',
+    justifyContent: 'center',
+
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  blueBox:{
+    backgroundColor:"blue",
+    width:"35px",
+    height:"35px"
+  },
+  redBox:{
+    backgroundColor:"red",
+    width:"35px",
+    height:"35px"
+  },
+  blackBox:{
+    backgroundColor:"black",
+    width:"35px",
+    height:"35px"
+  },
+  yellowBox:{
+    backgroundColor:"yellow",
+    width:"35px",
+    height:"35px"
+  },
+  greenBox:{
+    backgroundColor:"green",
+    width:"35px",
+    height:"35px"
+  },
+  whiteBox:{
+    backgroundColor:"#ffe4e1",
+    width:"35px",
+    height:"35px"
+  }
+});
